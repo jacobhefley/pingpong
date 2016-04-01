c708385031315fcf516869437f316d5ba4c1dfaf
jacobhefley <jacobhefley3@gmail.com>
2016-04-01 10:31:06 -0700

    reread directions fixed, basic ui

--- js/scripts.js
+++ js/scripts.js
@@ -3,19 +3,21 @@ $(function(event){
 		event.preventDefault();
 		var sentance = $("#sentance").val();
 		var num = parseInt(sentance);
-		if(num%3===0 && num%5!=0){
-			output="ping";
+		for(i=1; i<num+1; i++){
+			if(i%3===0 && i%5!=0){
+				output="ping";
+			}
+			else if(i%5===0 && i%3!=0){
+				output="pong";
+			}
+			else if(i%5===0 && i%3===0){
+				output="pingpong";
+			}
+			else{
+				output= i.toString();
+			}
+			var str = output;
+			$("#list").append("<li>"+str+"</li>");
 		}
-		else if(num%5===0 && num%3!=0){
-			output="pong";
-		}
-		else if(num%5===0 && num%3===0){
-			output="pingpong";
-		}
-		else{
-			output= sentance.toString();
-		}
-		var str = output;
-		$("#list").append("<li>"+str+"</li>");
-		});
+	});
 });
